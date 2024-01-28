d84d4e0e44a9952d45c88066545dc13507f6e2f2
Chloe Fabro <156484403+Chloemh48@users.noreply.github.com>
2024-01-27 18:14:11 -0800

    version1_readFile
    finished the read_file() function, this function will read originaltext.txt file from the computer

--- wp1.py
+++ wp1.py
@@ -1,1 +1,12 @@
 
+"""
+Chloe Fabro
+"""
+
+def read_file():
+    with open("originaltext.txt", "r") as myfile:
+        content = myfile.readlines()
+        for line in content:
+            line = line.strip()
+            print(line)
+def write_file_backwards():
