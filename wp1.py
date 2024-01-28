982eb9de723b4e0a053f8b97e28e06d0e15c1ddd
Chloe Fabro <156484403+Chloemh48@users.noreply.github.com>
2024-01-27 21:47:23 -0800

    deleted the read_file()
    delete the read_file() function. This is because it is reduandant

--- wp1.py
+++ wp1.py
@@ -3,21 +3,41 @@
 Chloe Fabro
 """
 
-def read_file():
+import sys
+import os
 
-    file = "riginaltext.txt"
-    try:
-        with open(file, "r") as myfile:
-            content = myfile.readlines()
-            for line in content:
-                line = line.strip()
-                print(line)
-    except FileNotFoundError:
-        print(f"{file} not exsit")
-    
-def write_file_backwards():
-    pass
+def write_file_backwards(file1, file2):
+
+    with open(file1, "r") as myfile:
+        content = myfile.readlines()
+
+    with open(file2, "w") as myfile2:
+        for line in reversed(content):
+            line = line.strip()
+            reversed_line = line[::-1]
+            myfile2.write(reversed_line + "\n")
+
+def main():
+    if len(sys.argv) != 3:
+        print('Usage: python wp1.py argument1 argument2')
+        sys.exit(1)  
+
+
+
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+
+    if not os.path.exists(file1): 
+        print(f'File {file1} does not exist.')
+        sys.exit(1)
+
+    if not os.path.exists(file2) : 
+        print(f'File {file2} does not exist.')
+        sys.exit(1)
+
+    write_file_backwards(file1, file2)
 
 if __name__ == "__main__":
-    read_file()
+    main()
+
 
