aa6dd998ade12de38e71716fe738bf8dbe2a737e
Chloe Fabro <156484403+Chloemh48@users.noreply.github.com>
2024-01-27 18:35:50 -0800

    add exception in read_file()
    if the given file path does not exsits, it will raise filenotfound error

--- wp1.py
+++ wp1.py
@@ -4,9 +4,20 @@ Chloe Fabro
 """
 
 def read_file():
-    with open("originaltext.txt", "r") as myfile:
-        content = myfile.readlines()
-        for line in content:
-            line = line.strip()
-            print(line)
+
+    file = "riginaltext.txt"
+    try:
+        with open(file, "r") as myfile:
+            content = myfile.readlines()
+            for line in content:
+                line = line.strip()
+                print(line)
+    except FileNotFoundError:
+        print(f"{file} not exsit")
+    
 def write_file_backwards():
+    pass
+
+if __name__ == "__main__":
+    read_file()
+
